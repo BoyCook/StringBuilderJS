02935e5e13895beccf6246aa771645020b795a62
Craig Cook <boycook@me.com>
2013-10-03 21:44:34 +0100

    Testing for string and array mode

--- test/StringBuilderSpec.js
+++ test/StringBuilderSpec.js
@@ -5,6 +5,21 @@ describe('StringBuilder', function () {
     describe('#appendString', function () {
         it('should append as string ok', function(){
             var sb = new StringBuilder();
+            sb.append('This');
+            sb.append('Is');
+            sb.append('Text');
+            var text = sb.toString();
+            text.should.eql('ThisIsText');
         })
     });            
+    describe('#appendArray', function () {
+        it('should append as array ok', function(){
+            var sb = new StringBuilder(true);
+            sb.append('This');
+            sb.append('Is');
+            sb.append('Text');
+            var text = sb.toString();
+            text.should.eql('ThisIsText');
+        })
+    });                
 });
